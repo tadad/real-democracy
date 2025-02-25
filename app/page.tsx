5229be5c357c5d1cdd4b37a6adb7e8f33a05ed26
Drew Tada <drew.tada@gmail.com>
2025-02-24 22:32:41 -0700

    edit(landingpage)

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,12 @@ import License from '@/components/License';
 
 const content = `
 Centralized institutions and paper-bound bureaucracy have choked the lifeblood of internet revolution.
-0xgov is the cutting edge of crypto government, exploring how blockchain, cryptocurrency, and decentralized tech shatter traditional state power.
-This is a publication to discuss, prototype, and broadcast the blueprint for sovereign internet-native governments and self-sovereign individuality.
+0xgov is the cutting edge of crypto government, exploring how blockchain, cryptocurrency, and decentralized tech replace the boomer-gov.
+We are a platform to discuss, prototype, and broadcast a new vision for sovereign internet-native governments and societies.
 
-0xgov Issue 1 drops in summer 2025
-
-Our vision is clear:
-- **Destruction of all 20th Century Bureaucray:** The complete digitization of society: currency, contracts, records—all transacted on immutable ledgers.
-- **Sovereign Individualism:** Document life beyond banks and states. Showcase how the internet redefines personal liberty.
-
+0xgov Issue 1 drops in summer 2025.
 Submit your articles, research, essays, and creative works to [Dachus](https://x.com/dac_hus) via dm.
-We want technical deep dives, visionary manifestos, and even speculative fiction that turns dystopia into decentralized possibility.
+We want technical deep dives, visionary manifestos, and speculative fiction that turns dystopia into possibility.
 `
 
 export default function Home() {
@@ -59,7 +54,6 @@ export default function Home() {
           <div></div>
           <MarkdownRenderer content={content} />
         </div>
-        <License />
       </div>
     </div>
   );
